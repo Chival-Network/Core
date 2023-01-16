911f3cf50574e34b88c25fea1d80be443da15793
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 14:52:41 +1300

    fisrt

--- src/main/java/au/chival/core/Core.java
+++ src/main/java/au/chival/core/Core.java
@@ -1,17 +1,26 @@
 package au.chival.core;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Core extends JavaPlugin {
 
+    public static JavaPlugin plugin;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        plugin = this;
+
+        //cmd-reg
+
+        //listener-reg
+
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        //
     }
 }
