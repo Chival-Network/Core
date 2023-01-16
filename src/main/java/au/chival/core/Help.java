e148410b01d55312bd2f997ed61bd44ccf50e454
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 16:18:19 +1300

    1.1 (updated pom)

--- src/main/java/au/chival/core/Help.java
+++ src/main/java/au/chival/core/Help.java
@@ -1,5 +1,6 @@
 package au.chival.core;
 
+import jdk.javadoc.internal.doclets.toolkit.taglets.snippet.Style;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -7,11 +8,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Help implements CommandExecutor, TabExecutor {
 
-    public void helpMessage(Player player) {
+    public void helpMessage(Player player, String[] args) {
 
         player.sendMessage(" ");
         player.sendMessage(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Welcome to Chival-Network®");
@@ -19,6 +21,16 @@ public class Help implements CommandExecutor, TabExecutor {
         player.sendMessage(ChatColor.DARK_AQUA + "Discord: " + ChatColor.DARK_GREEN + "https://discord.gg/kuz6EJpuZ9");
         player.sendMessage(ChatColor.DARK_AQUA + "Ask a staff member for more info");
         player.sendMessage(" ");
+
+        if (args[0] == "info") {
+            player.sendMessage(ChatColor.DARK_AQUA + "Info:");
+            player.sendMessage(" Loc: " + ChatColor.DARK_GREEN + player.getLocation());
+            player.sendMessage(" Rank: " + ChatColor.DARK_GREEN + );
+            player.sendMessage(" Display-name: " + ChatColor.DARK_GREEN + "");
+            player.sendMessage(" Ping: " + ChatColor.DARK_GREEN + "");
+            player.sendMessage(" TPS: " + ChatColor.DARK_GREEN + "");
+            player.sendMessage(" ");
+        }
 
     }
     @Override
@@ -29,13 +41,19 @@ public class Help implements CommandExecutor, TabExecutor {
             return false;
         }
 
-        helpMessage((Player) sender);
+        helpMessage((Player) sender, args);
 
         return true;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return null;
+        LinkedList<String> tab = new LinkedList();
+
+        if (args.length == 0) {
+            tab.add("info");
+        }
+
+        return tab;
     }
 }
