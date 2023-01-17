8604d4748afc0bfac5cd16925080bf8b68cd03f1
BuildTools <unconfigured@null.spigotmc.org>
2023-01-18 11:59:03 +1300

    v0.0.1 (added quit message)

--- src/main/java/au/chival/core/Core.java
+++ src/main/java/au/chival/core/Core.java
@@ -3,7 +3,6 @@ package au.chival.core;
 import au.chival.core.commands.Fly;
 import au.chival.core.commands.Help;
 import au.chival.core.commands.Speed;
-import au.chival.core.commands.TabCommand;
 import au.chival.core.listeners.Chat;
 import au.chival.core.listeners.Join;
 import au.chival.core.listeners.LuckpermEvents;
@@ -11,7 +10,6 @@ import net.luckperms.api.LuckPermsProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
-
 
 public final class Core extends JavaPlugin {
     public static JavaPlugin PLUGIN;
@@ -26,13 +24,16 @@ public final class Core extends JavaPlugin {
         new Help();
         new Fly();
         new Speed();
-        new TabCommand();
         //
 
         //listener-reg
         new LuckpermEvents(PLUGIN, LuckPermsProvider.get());
         getServer().getPluginManager().registerEvents(new Join(), PLUGIN);
         getServer().getPluginManager().registerEvents(new Chat(), PLUGIN);
+        //
+
+        //other
+
         //
 
         Logger.getLogger("Chival").info("ChivalCore v" + this.getDescription().getVersion() + " successfully loaded");
