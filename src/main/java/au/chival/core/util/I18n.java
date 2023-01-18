90625ab08e6a098db571722a91a4b24b7f4db97e
UltimateGG <clubblock1@gmail.com>
2023-01-17 20:25:37 -0600

    Load bundle externally for config

--- src/main/java/au/chival/core/util/I18n.java
+++ src/main/java/au/chival/core/util/I18n.java
@@ -1,13 +1,27 @@
 package au.chival.core.util;
 
+import au.chival.core.Core;
 import org.bukkit.ChatColor;
 
+import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
 
 public class I18n {
-	private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("messages");
+	private static ResourceBundle BUNDLE;
+	static {
+		try { // Load the resource bundle from plugin data dir
+			URL[] urls = new URL[] { Core.PLUGIN.getDataFolder().toURI().toURL() };
+			ClassLoader loader = new java.net.URLClassLoader(urls);
+			BUNDLE = ResourceBundle.getBundle("messages", Locale.ENGLISH, loader);
+		} catch (Exception e) { // Fall back and use default messages.properties from inside the jar
+			Core.PLUGIN.saveResource("messages.properties", false);
+			BUNDLE = ResourceBundle.getBundle("messages");
+		}
+	}
+
 	public static final String INFO = ChatColor.translateAlternateColorCodes('&', get("info"));
 	public static final String ERROR = ChatColor.translateAlternateColorCodes('&', get("error"));
 	public static final String SUCCESS = ChatColor.translateAlternateColorCodes('&', get("success"));
